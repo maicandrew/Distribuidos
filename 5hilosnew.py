43e94567f0532fc62abe47a9b8172e8c65fbaf34
Makiol <michaelgandresg@gmail.com>
2019-11-24 00:17:54 -0500

    Agregados todos los comandos de las salas, falta por agregar: * Registro * Inicio de sesión * Sincronización de relojes * Cliente

--- 5hilosnew.py
+++ 5hilosnew.py
@@ -1,48 +1,165 @@
 import threading
 from socket import socket, error
 clientes = {}
-names = []
+salas = {}
 #import time
 
+class Room(threading.Thread):
+    def __init__(self, name, owner = None):
+        super(Room, self).__init__()
+        self.owner = owner
+        self.name = name
+        self.users = []
+        self.users.append(owner)
+
+    def run(self):
+        while len(self.users) > 0:
+            for user in self.users:
+                if user.message != "":
+                    if user.priv == None:
+                        cad = user.name+": "+user.message
+                        self.send(cad.encode())
+                    else:
+                        cad = "/private "+user.name+": "+user.message
+                        user.priv.conn.send(cad.encode())
+                    user.message = ""
+                if user.command != "":
+                    comm = user.command
+                    if comm.startswith("#cR "):
+                        createRoom(comm[4:], user)
+                    elif comm.startswith("#gR "):
+                        getRoom(comm[4:], user)
+                    elif comm == "#eR":
+                        exitRoom(user)
+                    elif comm == "exit":
+                        exitClient(user)
+                    elif comm == "#lR":
+                        listRooms(user)
+                    elif comm.startswith("#dR "):
+                        deleteRoom(comm[4:], user)
+                    elif comm == "#show users":
+                        getUsuarios(user)
+                    elif comm.startswith("#/private "):
+                        sendPrivate(comm[10:], user)
+                    else:
+                        user.conn.send("Comando no reconocido".encode())
+                    user.command = ""
+                    #condicional para comandos
+        print("Sala", self.name, "vacía. Cerrando sala")
+
+    def send(self, message):
+        for user in self.users:
+            user.conn.send(message.encode())
+
 class Cliente(threading.Thread):
-    def __init__(self, conn, addr, name, peer = None):
+    def __init__(self, conn, addr, name, room = "default"):
         super(Cliente, self).__init__()
         self.conn = conn
         self.addr = addr
         self.name = name
-        self.peer = peer
+        self.message = ""
+        self.command = ""
+        self.priv = None
+        self.room = room
 
     def run(self):
         while self.conn:
-            if self.peer == None:
-                print("El cliente debe seleccionar con quien chatear")
-                nombres = str(names)
-                conn.send(nombres.encode())
-                print("Seleccionando chat")
-                peer = self.conn.recv(1024).decode()
-                if clientes[peer][0].peer == None:
-                    self.peer = clientes[peer][0]
-                    clientes[peer][0].peer = self
-                    self.conn.send("yes".encode())
-                    print("Cliente seleccionado")
+            data = self.conn.recv(1024).decode()
+            if data:
+                if data.startswith("#"):
+                    self.command = data
                 else:
-                    self.conn.send("no".encode())
+                    self.message = data
             else:
-                data = self.conn.recv(1024).decode()
-                self.peer.conn.send(data)
-                if data =="Salir" or not data:
-                    self.conn.send("s".encode())
-                    break
-                    print(data, self.addr)
-                    self.conn.send(data.encode())
-        print("Sesión finalizada: ",self.addr)
+                break
+        print("Sesión finalizada:",self.name, self.addr)
         clientes[self.addr][1] = 0
 
+    def join(self):
+        self.conn.send("Cerrando conexión".encode())
+        self.conn.close()
+        self.room = None
+        threading.Thread.join(self)
+
+def createRoom(name, user = None):
+    if not name in salas:
+        sala = Room(name, user)
+        sala.start()
+        salas[name] = sala
+        user.room = sala
+        cad = "Sala creada: "+name
+        user.conn.send(cad.encode())
+    else:
+        user.conn.send("Error al intentar crear la sala".encode())
+
+def getRoom(name, user):
+    if name in salas:
+        sala = salas[name]
+        sala.users.append(user)
+        user.room = sala
+        cad = "Conectado a "+name
+        user.conn.send(cad.encode())
+    else:
+        user.conn.send("Error al intentar conectarse a la sala".encode())
+
+def exitRoom(user):
+    sala = user.room
+    if sala.name != "default":
+        sala.users.remove(user)
+        user.room(salas["default"])
+        sala["default"].users.append(user)
+        user.conn.send("Saliendo a la sala por defecto".encode())
+
+def exitClient(user):
+    sala = user.room
+    sala.users.remove(user)
+    clientes.pop(user.name)
+    user.join()
+
+def listRooms(user):
+    cad = ""
+    for sala in salas:
+        cad += sala+": "
+        cant = len(salas[sala].users)
+        if sala == "default":
+            cant-=1
+        cad += str(cant) + " usuarios"
+        cad +="\n"
+    cad = cad[:-1]
+    user.conn.send(cad.encode())
+
+def deleteRoom(name, user):
+    if name in salas:
+        sala = salas[name]
+        if sala.owner == user.name:
+            for u in sala.users:
+                exitRoom(u)
+            cad = "Sala " + name + " borrada"
+            user.conn.send(cad.encode())
+        else:
+            cad = "No eres el dueño de la sala "+name
+            user.conn.send(cad.encode())
+    else:
+        cad = "Error al intentar borrar la sala "+name
+        user.conn.send(cad.encode())
+
+def getUsuarios(user):
+    cad = ""
+    for user in clientes:
+        cad += user+"\n"
+    cad = cad[:-1]
+    user.conn.send(cad.encode())
+
+def sendPrivate(target, source):
+    if target in clientes:
+        source.priv = clientes[target]
 
 if __name__ == "__main__":
     s = socket()
     s.bind(("localhost", 8000))
     s.listen(5)
+    createRoom("default")
+    """
     while True:
         print("En espera de conexión...")
         conn, addr = s.accept()
@@ -64,3 +181,4 @@ if __name__ == "__main__":
                 clientes.pop(i)
                 print(clientes)
                 break
+    """
