7bd46c36ae1167aa6e5bea31e6cc9631f1f160be
Makiol <michaelgandresg@gmail.com>
2019-11-24 00:17:54 -0500

    Agregados todos los comandos de las salas, falta por agregar: * Registro * Inicio de sesión * Sincronización de relojes * Cliente

--- cliente.py
+++ cliente.py
@@ -9,50 +9,60 @@ class listener(threading.Thread):
     def run(self):
         while self.sock:
             message = self.sock.recv(1024)
-            print(peer+":", message)
+            print(message)
 
-s = socket.socket()
-s.connect(("localhost", 8000))
-name = ""
-peer = ""
-while True:
-    names = s.recv(1024).decode()[1:-1].split(", ")
-    while name == "":
-        name = input("Ingrese un nombre para regitrarse: ")
-    if not repr(name) in names:
-        print("Nombre válido.")
-        s.send(name.encode())
-        break
-    else:
-        print("Nombre en uso, elija otro.")
+def login(s):
+    return 0
+#    while True:
+#        pass
 
-while True:
-    names = s.recv(1024).decode()[1:-1].split(", ")
-    for n in names:
-        if n[1:-1] != name:
-            print (n[1:-1])
-    peer = input("Seleccione con quien chatear: ")
-    while peer == "":
-        peer = input("Seleccione con quien chatear: ")
-    if repr(peer) in names:
-        s.send(peer.encode())
-        r = s.recv(10).decode()
-        if r == "yes":
-            print("Chat seleccionado")
-            break
+def name_select(s, name):
+    while True:
+        names = s.recv(1024).decode()[1:-1].split(", ")
+        while name == "":
+            name = input("Ingrese un nombre para regitrarse: ")
+        if not repr(name) in names:
+            print("Nombre válido.")
+            s.send(name.encode())
+            return name
         else:
-            print("El usuario ya se encuentra conectado con alguien más. Intente con otro nombre")
-    else:
-        print("Nombre inválido")
+            print("Nombre en uso, elija otro.")
 
-while True:
-    l = listener(s)
-    l.start()
-    a = input(name)
-    while a == "":
+def peer_select(s):
+    while True:
+        names = s.recv(1024).decode()[1:-1].split(", ")
+        for n in names:
+            if n[1:-1] != name:
+                print (n[1:-1])
+        peer = input("Seleccione con quien chatear: ")
+        while peer == "":
+            peer = input("Seleccione con quien chatear: ")
+        if repr(peer) in names:
+            s.send(peer.encode())
+            r = s.recv(10).decode()
+            if r == "yes":
+                print("Chat seleccionado")
+                break
+            else:
+                print("El usuario ya se encuentra conectado con alguien más. Intente con otro nombre")
+        else:
+            print("Nombre inválido")
+
+
+if __name__ == "__main__":
+    s = socket.socket()
+    s.connect(("localhost", 8000))
+    name = ""
+    peer = ""
+    name = login(s)
+    while True:
+        l = listener(s)
+        l.start()
         a = input(name)
-    s.send(a.encode())
-    print(a)
-    if a == "s":
-        s.close()
-        break
+        while a == "":
+            a = input(name)
+        s.send(a.encode())
+        print(a)
+        if a == "s":
+            s.close()
+            break
